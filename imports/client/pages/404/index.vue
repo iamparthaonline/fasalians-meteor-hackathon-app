e8f7a049e01b09c47ee9f359d3b5c15e4505efa0
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2022-09-24 21:03:27 +0530

    added error page, top / bottom bar

--- imports/client/pages/404/index.vue
+++ imports/client/pages/404/index.vue
@@ -1,9 +1,52 @@
 <template>
-  <h2>404 NOT FOUND</h2>
+  <div>
+    <TopNavBar
+      title="Err.."
+    />
+    <div class="error-page">
+      <img height="200px" width="200px" src="/road.svg">
+      <p>
+        It seems something went wrong, please click below to find the right way!
+      </p>
+        <v-btn class="ma-2" outlined color="secondary" @click="$router.push({name: 'dashboard'})">
+          <v-icon left>mdi-arrow-left</v-icon>
+          Go to Dashboard
+        </v-btn>
+    </div>
+    <BottomNavBar active-page="dashboard" />
+  </div>
 </template>
 
 <script>
-  export default {};
+  import BottomNavBar from '../../components/bottomBar.vue';
+  import TopNavBar from '../../components/topNavBar.vue';
+
+  export default {
+    components: {
+      BottomNavBar,
+      TopNavBar,
+    },
+    meteor: {
+      user: () => Meteor.user(),
+    },
+  };
 </script>
 
-<style></style>
+<style lang="scss">
+  .error-page {
+    padding: 20px;
+    padding-bottom: 100px;
+    margin: auto;
+    text-align: center;
+    margin-top: 50px;
+  }
+  img{
+    margin: 20px !important;
+    display: inline-block;
+  }
+  p {
+    margin: 20px 0 !important;
+  }
+
+
+</style>
